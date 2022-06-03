acc584754b21df3fb2811831641e9f30cb763e21
Guillaume <guillaume.waroux@gmail.com>
2022-06-03 11:06:03 +0200

    update script create

--- scripts/create_new_infra.sh
+++ scripts/create_new_infra.sh
@@ -17,6 +17,10 @@ if [ -d "INFRASTRUCTURE" ]; then
 else
     mkdir INFRASTRUCTURE
     cd INFRASTRUCTURE
-    mkdir 1
-    echo "Your client ID is 1, don't forget this !"
+    id_new_infra=1
+    mkdir $id_new_infra
+    echo "Your client ID is $id_new_infra, don't forget this !"
 fi
+
+cp -R ../0* ${id_new_infra}
+cp ../flow_matrix.csv ${id_new_infra}
